6775ff14b0d274b906aa740ff697750d19083f82
smallwordWISH <word714cs@gmail.com>
2017-11-25 03:24:59 +0800

    add kaminari gem and change index view and controller index

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -6,7 +6,7 @@ class Admin::RestaurantsController < ApplicationController
 
 
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.page(params[:page]).per(10)
   end
 
   def new
