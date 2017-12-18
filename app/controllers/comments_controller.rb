d1dba55098c2976376bef0e47dce8512add35339
smallwordWISH <word714cs@gmail.com>
2017-12-19 00:19:38 +0800

    edit show index let user can read comments on restaurant show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:content)
   end
 end
