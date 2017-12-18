3ce518d94c56b609b5fc0a9a1e5b128783268ee1
smallwordWISH <word714cs@gmail.com>
2017-12-18 23:44:03 +0800

    add comment controller create that user can add comment on restaurtrant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])    
+    @restaurant = Restaurant.find(params[:id])
+    @comment = Comment.new    
   end
 end
