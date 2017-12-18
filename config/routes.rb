0fac4ba5b21292d10afbc06bdacc5183b8f90d07
smallwordWISH <word714cs@gmail.com>
2017-12-18 21:48:14 +0800

    create comment routes and comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
 
   root "restaurants#index"
 
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do 
+    resources :comments, only: [:create, :destroy] 
+  end
+
   resources :categories, only: :show
 
   namespace :admin do
     resources :restaurants
     resources :categories
     root "restaurants#index"
-
   end
 
 end
