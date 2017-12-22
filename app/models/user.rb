54f412ee4ba7a37f192cea5557f3000b8a7ef08e
smallwordWISH <word714cs@gmail.com>
2017-12-22 22:23:24 +0800

    add commented restaurants on user show view and can link to restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :comments
+  has_many :restaurants, through: :comments
 
   validates_presence_of :name
 
